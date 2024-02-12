a0302f863205fcc84443d5b2148e8c4617591ab2
SakaueYae <y.sakaue@4nonome.com>
2024-02-12 21:46:02 +0900

    department_role_memberのindex追加

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,2 +1,4 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 alter table user add index id_index (mail, password);
+alter table department_role_member add index belong_index (belong);
+alter table department_role_member add index role_id_index (role_id);
