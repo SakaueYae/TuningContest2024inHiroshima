e0c204a40d4dfcd18c0660e2f567c0f9f1ad4967
kmmpy <mutsukikasa0120@gmail.com>
2024-02-13 14:13:47 +0900

    iiii

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,7 +1,6 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 alter table user add index myUindex1 (mail, password);
+alter table user add index myUindex2 (entry_date, kana);
+alter table user add index myUindex3 (goal);
+alter table match_group_member add index myMindex1(user_id);
 alter table department_role_member add index myDindex1 (belong, role_id);
-
--- ここから追記
---alter table match_group_member add index myindex1(user_id);
---alter table user add index myindex2 (entry_date, kana);
