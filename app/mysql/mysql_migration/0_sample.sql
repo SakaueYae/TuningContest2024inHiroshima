2b06767d1b93c0f8f1c393b7ec1a3bde2d005a4d
kmmpy <mutsukikasa0120@gmail.com>
2024-02-13 12:14:07 +0900

    index

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,8 +1,7 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-alter table user add index id_index (mail, password);
-alter table department_role_member add index belong_index (belong);
-alter table department_role_member add index role_id_index (role_id);
+alter table user add index myUindex1 (mail, password);
+alter table department_role_member add index myDindex1 (belong, role_id);
 
 -- ここから追記
-alter table match_group_member add index myindex1(user_id);
-alter table user add index myindex2 (entry_date, kana);
+--alter table match_group_member add index myindex1(user_id);
+--alter table user add index myindex2 (entry_date, kana);
