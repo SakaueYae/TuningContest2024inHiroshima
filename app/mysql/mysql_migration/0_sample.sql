98f2ca75fe9830f578cd23e6a5032d36cb6dd042
kmmpy <mutsukikasa0120@gmail.com>
2024-02-13 10:24:20 +0900

    index

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -2,3 +2,7 @@
 alter table user add index id_index (mail, password);
 alter table department_role_member add index belong_index (belong);
 alter table department_role_member add index role_id_index (role_id);
+
+-- ここから追記
+alter table match_group_member add index myindex1(user_id);
+alter table user add index myindex2 (entry_date, kana);
